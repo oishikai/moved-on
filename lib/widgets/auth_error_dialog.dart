3ed69dd65832b18c0373ba70a3c2ca664c997554
Kai Oishi <bokuchan.kai@gmail.com>
2023-08-20 04:56:01 +0900

    エラー表示修正

--- lib/widgets/auth_error_dialog.dart
+++ lib/widgets/auth_error_dialog.dart
@@ -1,12 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthErrorDialog extends StatelessWidget {
   const AuthErrorDialog({Key? key, required this.error}) : super(key: key);
 
-  final Object error;
+  final error;
 
   @override
   Widget build(BuildContext context) {
+    String message = '';
+
+    if (error is FirebaseAuthException) {
+      switch ((error as FirebaseAuthException).code) {
+        case 'invalid-email':
+          message = 'メールアドレスが不正です';
+          break;
+        case 'user-disabled':
+          message = 'ユーザーが無効です';
+          break;
+        case 'user-not-found':
+          message = 'ユーザーが見つかりません';
+          break;
+        case 'wrong-password':
+          message = 'パスワードが間違っています';
+          break;
+        case 'email-already-in-use':
+          message = 'メールアドレスは既に使用されています';
+          break;
+        case 'operation-not-allowed':
+          message = 'この操作は許可されていません';
+          break;
+        case 'weak-password':
+          message = 'パスワードが弱すぎます';
+          break;
+        default:
+          message = 'エラーが発生しました';
+      }
+    } else {
+      message = 'エラーが発生しました';
+    }
+
     return AlertDialog(
       backgroundColor: Theme.of(context).colorScheme.errorContainer,
       title: Icon(
@@ -14,7 +47,14 @@ class AuthErrorDialog extends StatelessWidget {
         color: Theme.of(context).colorScheme.error,
         size: 48,
       ),
-      content: Text(error.toString()),
+      content: Text(
+        message,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.onErrorContainer,
+          fontSize: 16,
+        ),
+      ),
       actionsAlignment: MainAxisAlignment.center,
       actions: [
         SizedBox(
