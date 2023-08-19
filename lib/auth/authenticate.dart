91a83c8c09c1f6541b75036ce59b460962a734d7
Kai Oishi <bokuchan.kai@gmail.com>
2023-08-20 00:00:55 +0900

    Authエラーenum追加

--- lib/auth/authenticate.dart
+++ lib/auth/authenticate.dart
@@ -1,5 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+enum AuthResultStatus {
+  weakPassword('パスワードが簡単すぎます'),
+  emailAlreadyInUse('すでに使用されているメールアドレスです'),
+  invalidEmail('メールアドレスが無効です'),
+  userNotFound('ユーザーが存在しません'),
+  wrongPassword('パスワードが間違っています'),
+  unknown('不明なエラーです');
+
+  final String message;
+  const AuthResultStatus(this.message);
+}
+
 class Authenticate {
   static void createAccount(String email, String pass) async {
     try {
@@ -10,47 +22,31 @@ class Authenticate {
       print('アカウント作成成功');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
-        print('パスワードが弱すぎます');
+        throw AuthResultStatus.weakPassword;
       } else if (e.code == 'email-already-in-use') {
-        print('すでに使用されているメールアドレスです');
-      } else {
-        print('アカウント作成エラー');
+        throw AuthResultStatus.emailAlreadyInUse;
       }
     } catch (e) {
-      print(e);
+      throw AuthResultStatus.unknown;
     }
   }
 
   static void logIn(String email, String pass) async {
     try {
-      /// credential にはアカウント情報が記録される
       final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: email,
         password: pass,
       );
       print('サインイン成功');
-    }
-
-    /// サインインに失敗した場合のエラー処理
-    on FirebaseAuthException catch (e) {
-      /// メールアドレスが無効の場合
+    } on FirebaseAuthException catch (e) {
       if (e.code == 'invalid-email') {
-        print('メールアドレスが無効です');
-      }
-
-      /// ユーザーが存在しない場合
-      else if (e.code == 'user-not-found') {
-        print('ユーザーが存在しません');
-      }
-
-      /// パスワードが間違っている場合
-      else if (e.code == 'wrong-password') {
-        print('パスワードが間違っています');
-      }
-
-      /// その他エラー
-      else {
-        print('サインインエラー');
+        throw AuthResultStatus.invalidEmail;
+      } else if (e.code == 'user-not-found') {
+        throw AuthResultStatus.userNotFound;
+      } else if (e.code == 'wrong-password') {
+        throw AuthResultStatus.wrongPassword;
+      } else {
+        throw AuthResultStatus.unknown;
       }
     }
   }
